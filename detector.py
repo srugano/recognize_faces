c6bc8369642ca2ee9d6c00399c11d42b8ccdf5ab
Allan Stockman Rugano <kuristockyrugano@gmail.com>
2023-12-19 04:31:20 +0100

    Better arguments

--- detector.py
+++ detector.py
@@ -2,11 +2,28 @@
 import pickle
 from collections import Counter
 from pathlib import Path
-
+import argparse
 import face_recognition
 
 # Define the path for storing face encodings
 DEFAULT_ENCODINGS_PATH = Path("output/encodings.pkl")
+
+# Set up command-line argument parsing
+parser = argparse.ArgumentParser(description="Recognize faces in an image")
+parser.add_argument("--train", action="store_true", help="Train on input data")
+parser.add_argument("--validate", action="store_true", help="Validate trained model")
+parser.add_argument("--test", action="store_true", help="Test the model with an unknown image")
+parser.add_argument(
+    "-m",
+    action="store",
+    default="hog",
+    choices=["hog", "cnn"],
+    help="Which model to use for training: hog (CPU), cnn (GPU)",
+)
+
+# Add argument for specifying the file path for testing
+parser.add_argument("-f", action="store", help="Path to an image with an unknown face")
+args = parser.parse_args()
 
 # Create necessary directories if they do not exist
 Path("training").mkdir(exist_ok=True)
@@ -19,8 +36,14 @@ def encode_known_faces(model: str = "hog", encodings_location: Path = DEFAULT_EN
     names = []  # List to store names of individuals
     encodings = []  # List to store face encodings
 
-    # Loop through each file in the training directory
-    for filepath in Path("training").glob("*/*"):
+    # Get all image filepaths from the training directory
+    image_filepaths = list(Path("training").glob("*/*"))
+    total_images = len(image_filepaths)  # Total number of images
+
+    print("[INFO] quantifying faces...")
+
+    for idx, filepath in enumerate(image_filepaths, start=1):
+        print(f"[INFO] processing image {idx}/{total_images}")
         name = filepath.parent.name  # Extract the name from the directory structure
         image = face_recognition.load_image_file(filepath)  # Load the image
         face_locations = face_recognition.face_locations(image, model=model)  # Detect faces in the image
@@ -37,6 +60,8 @@ def encode_known_faces(model: str = "hog", encodings_location: Path = DEFAULT_EN
     # Write the encodings to a file
     with encodings_location.open(mode="wb") as f:
         pickle.dump(name_encodings, f)
+
+    print("[INFO] serializing encodings...")
 
 
 # Helper function to recognize a single face
@@ -76,6 +101,30 @@ def recognize_faces(image_location: str, model: str = "hog", encodings_location:
         print(name, bounding_box)
 
 
-# Example usage
-# encode_known_faces()
-recognize_faces("unknown.jpg")
+# Function to validate face recognition on a set of images
+def validate(model: str = "hog"):
+    """
+    This function iterates through all files in the 'validation' directory
+    and uses the 'recognize_faces' function to perform face recognition on each file.
+    It allows testing the face recognition system on a separate dataset (validation dataset).
+
+    Args:
+    model (str): The model used for face detection. Defaults to "hog".
+                 Other options can be "cnn", which might be more accurate but is slower.
+    """
+    # Iterate over all files in the 'validation' directory and its subdirectories
+    for filepath in Path("validation").rglob("*"):
+        # Check if the current path is a file
+        if filepath.is_file():
+            # Call the recognize_faces function on the file
+            # 'filepath.absolute()' is used to get the absolute path of the file
+            recognize_faces(image_location=str(filepath.absolute()), model=model)
+
+
+if __name__ == "__main__":
+    if args.train:
+        encode_known_faces(model=args.m)
+    if args.validate:
+        validate(model=args.m)
+    if args.test:
+        recognize_faces(image_location=args.f, model=args.m)
